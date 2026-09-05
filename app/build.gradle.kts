ab4bec5307bf64eaefd1936c9df75f7b8a48f030
AniviSmp <anuragsinghgjp@gmail.com>
2026-09-05 21:04:13 +0530

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "com.anivi.smp"
+
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.anivi.smp"
+
+        minSdk = 23
+
+        targetSdk = 36
+
+        versionCode = 1
+
+        versionName = "1.0"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(platform("androidx.compose:compose-bom:2026.08.00"))
+
+    implementation("androidx.activity:activity-compose:1.13.0")
+
+    implementation("androidx.compose.ui:ui")
+
+    implementation("androidx.compose.ui:ui-tooling-preview")
+
+    implementation("androidx.compose.material3:material3")
+
+    implementation("androidx.compose.material:material-icons-extended")
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
+}
